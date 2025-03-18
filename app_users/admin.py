f3102298e9e4f56d62c39a48225c6654e422d675
Imronbek <imronbaxtiyorov39@gmail.com>
2025-03-18 21:34:19 +0500

    commit

--- app_users/admin.py
+++ app_users/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
-from app_users.models import *
-
-admin.site.register([User,Student,Teacher,Parent,HomeworkHistory])
-from django.contrib import admin
-
 # Register your models here.
--- app_users/serializers.py
+++ app_users/serializers.py
@@ -1,62 +1,34 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from django.contrib.auth.hashers import make_password
-from app_users.models import Teacher, User, Student, Parent
-
-
-class UserAllSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
+from .models import Teacher, Student
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "password", "full_name", "phone")
-
-    def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        return super().create(validated_data)
-
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class TeacherSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
+    user = UserSerializer()
 
     class Meta:
         model = Teacher
-        fields = ('id', 'user', 'cource', 'description')
+        fields = ['id', 'user', 'subject', 'experience']
 
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        teacher = Teacher.objects.create(user=user, **validated_data)
+        return teacher
 
-class StudentSerializer(serializers.ModelSerializer): #Talabalar uchun serializer
-    user = serializers.PrimaryKeyRelatedField(read_only=True)
+class StudentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
 
     class Meta:
         model = Student
-        fields = ("id", "user", "group", "cource", "description")
+        fields = ['id', 'user', 'birth_date', 'grade']
 
-
-class ParentSerializer(serializers.ModelSerializer):
-    students = StudentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Parent
-        fields = ('id', 'name', 'surname', 'address', 'phone', 'description', 'students')
-
-
-class GetStudentsByIdsSerializer(serializers.Serializer):
-    student_ids = serializers.ListField(child=serializers.IntegerField())
-
-
-class GetTeachersByIdsSerializer(serializers.Serializer):
-    teacher_ids = serializers.ListField(child=serializers.IntegerField())
-
-
-class UserAndTeacherSerializer(serializers.Serializer):
-    user = UserSerializer()
-    teacher = TeacherSerializer()
-
-
-class UserAndStudentSerializer(serializers.Serializer):
-    user = UserSerializer()
-    student = StudentSerializer()
-
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        student = Student.objects.create(user=user, **validated_data)
+        return student
